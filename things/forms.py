fdfdcd14e9520f4e71729c14e1d1fa0ba41eb579
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-24 20:43:14 +0100

    Done 1.3

--- things/forms.py
+++ things/forms.py
@@ -6,16 +6,3 @@ class ThingForm(forms.ModelForm):
         model = Thing
         fields = ['name', 'description', 'quantity']
         exclude = {'created_at'}
-
-
-    def clean_quantity(self):
-            quantity = self.cleaned_data.get('quantity')
-            if quantity < 0:
-                raise forms.ValidationError('Quantity must be a non-negative number.')
-            return quantity
-        
-    def clean_name(self):
-        name = self.cleaned_data.get('name')
-        if Thing.objects.filter(name=name).exists():
-            raise forms.ValidationError('This name is already in use.')
-        return name
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+def clean_quantity(self):
+            quantity = self.cleaned_data.get('quantity')
+            if quantity < 0:
+                raise forms.ValidationError('Quantity must be a non-negative number.')
+            return quantity
+        
+def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if Thing.objects.filter(name=name).exists():
+            raise forms.ValidationError('This name is already in use.')
+        return name
