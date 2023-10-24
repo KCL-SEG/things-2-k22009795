43c4f024c843e6f299fb72c389543c150d10797b
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-24 20:29:07 +0100

    Done

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,21 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        exclude = {'created_at'}
+
+
+    def clean_quantity(self):
+            quantity = self.cleaned_data.get('quantity')
+            if quantity < 0:
+                raise forms.ValidationError('Quantity must be a non-negative number.')
+            return quantity
+        
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if Thing.objects.filter(name=name).exists():
+            raise forms.ValidationError('This name is already in use.')
+        return name
